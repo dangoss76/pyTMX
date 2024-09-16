eabd248fcc68721451331e5bf73df2c8a2a6cbfe
dangoss76 <dangoss.perso@gmail.com>
2024-09-16 11:16:55 -0400

    added comments

--- quotes_ff.py
+++ quotes_ff.py
@@ -24,15 +24,15 @@ for q1, q2 in zip(quoteOptions, quoteCSV):
 
     for index, row in data.iterrows():
 
-        element = browser.find_element(By.ID,'page-content')
-        browser.execute_script("arguments[0].scrollIntoView();", element)
-        dropdown_equity = browser.find_element(By.ID,(f'{q1}'))
+        element = browser.find_element(By.ID,'page-content') # Find the top element of the page
+        browser.execute_script("arguments[0].scrollIntoView();", element) # We scroll back to top 
+        dropdown_equity = browser.find_element(By.ID,(f'{q1}')) # we select a quotes in the dropdown
         select = Select(dropdown_equity)
         select.select_by_value(row['symbol'])
 
-        clickOkBtn = browser.find_element(By.XPATH,"//select[@id='" +f'{q1}'+ "']/following-sibling::div").click()
+        clickOkBtn = browser.find_element(By.XPATH,"//select[@id='" +f'{q1}'+ "']/following-sibling::div").click() # Click OK button next to the select
 
-        quoteResult = browser.find_element(By.TAG_NAME,'h2').text
+        quoteResult = browser.find_element(By.TAG_NAME,'h2').text # Validate if title match
         assert quoteResult== row['title']
 
         print(row['symbol'], row['title'])
